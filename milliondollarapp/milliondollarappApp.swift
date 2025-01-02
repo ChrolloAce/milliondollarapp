c94202a48f571c2c8eadd36eccc7c8379a462972
w <legion25@gma>
2025-01-02 00:58:09 -0500

    Initial Commit

---
+++ milliondollarapp/milliondollarappApp.swift
@@ -0,0 +1,17 @@
+//
+//  milliondollarappApp.swift
+//  milliondollarapp
+//
+//  Created by Ernesto  Lopez on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct milliondollarappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
